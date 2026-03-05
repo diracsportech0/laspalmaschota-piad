389d533c59da71083e59a23404c5406637ce09d0
diracsportech0 <dirac.sportech@gmail.com>
2026-03-04 20:27:39 -0500

    etl no importar pases

--- pages/3_🚹Mi_Plantel.py
+++ pages/3_🚹Mi_Plantel.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from etl import df, df_pass
+from etl import df#, df_pass
 from functions import player_passmap,graph_barras, heat_map
 
 st.title("🚹 MI PLANTEL")
